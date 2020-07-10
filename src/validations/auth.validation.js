c071e3f8ae5dd703f7398b7e7a8eeaca1af41950
Redeemer40 <victorjonah199@gmail.com>
2020-07-10 13:56:25 +0100

    add validation for phone number & username

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -3,9 +3,11 @@ const { password } = require('./custom.validation');
 
 const register = {
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().custom(password),
     name: Joi.string().required(),
+    username: Joi.string().required(),
+    email: Joi.string().required().email(),
+    phoneNumber: Joi.string().required(),
+    password: Joi.string().required().custom(password),
   }),
 };
 
