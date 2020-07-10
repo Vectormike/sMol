fa7bad27bf84cbf24add7c736cad39922c540465
Redeemer40 <victorjonah199@gmail.com>
2020-07-10 13:54:34 +0100

    add user controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -29,15 +29,9 @@ const updateUser = catchAsync(async (req, res) => {
   res.send(user);
 });
 
-const deleteUser = catchAsync(async (req, res) => {
-  await userService.deleteUserById(req.params.userId);
-  res.status(httpStatus.NO_CONTENT).send();
-});
-
 module.exports = {
   createUser,
   getUsers,
   getUser,
   updateUser,
-  deleteUser,
 };
