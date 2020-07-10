48b75587bc78ac7abe20042e83204b36ab37c863
Redeemer40 <victorjonah199@gmail.com>
2020-07-10 17:06:43 +0100

    ...

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -1,19 +1,18 @@
 const { version } = require('../../package.json');
-const config = require('../config/config');
 
 const swaggerDef = {
   openapi: '3.0.0',
   info: {
-    title: 'node-express-mongoose-boilerplate API documentation',
+    title: 'Servit API documentation',
     version,
     license: {
       name: 'MIT',
-      url: 'https://github.com/hagopj13/node-express-mongoose-boilerplate/blob/master/LICENSE',
+      url: 'https://github.com/Vectormike/service-app',
     },
   },
   servers: [
     {
-      url: `http://localhost:${config.port}/v1`,
+      url: `https://servit-api.herokuapp.com/v1`,
     },
   ],
 };
