da5ef1c571aa0ca4fc55d237f497fdfaecd02a73
Redeemer40 <victorjonah199@gmail.com>
2020-07-12 20:31:21 +0100

    ...

--- src/routes/v1/geolocation.route.js
+++ src/routes/v1/geolocation.route.js
@@ -5,3 +5,42 @@ const geolocationController = require('../../controllers/geolocation.controller'
 const router = express.Router();
 
 router.post('/', auth('user'), geolocationController.getUserLocationInformation);
+
+module.exports = router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Geolocation
+ *   description: Find the user's location information
+ */
+
+/**
+ * @swagger
+ * path:
+ *  /location:
+ *    post:
+ *      summary: Input ip address
+ *      tags: [Location]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              required:
+ *                - ip Address
+ *              properties:
+ *                ip Address:
+ *                  type: string
+ *              example:
+ *                ip Address: Joe
+ *      responses:
+ *        "200":
+ *          description: Accepted
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *
+ */
