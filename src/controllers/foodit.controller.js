32ba1ef0cab7186073ea7cf824836479e780e836
emex4gman <emex4gman@gmail.com>
2020-07-16 00:36:32 +0100

    find all foods

--- src/controllers/foodit.controller.js
+++ src/controllers/foodit.controller.js
@@ -8,5 +8,9 @@ const createFood = catchAsync(async (req, res) => {
   const food = await fooditService.createFood(req.body);
   res.status(httpStatus.CREATED).json({ food });
 });
+const getFoods = catchAsync(async (req, res) => {
+  const foods = await fooditService.getFoods();
+  res.status(httpStatus.CREATED).json({ foods });
+});
 
-module.exports = { createFood };
+module.exports = { createFood, getFoods };
