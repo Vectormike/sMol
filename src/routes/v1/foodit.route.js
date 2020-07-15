6b31111088bae93c35e2f3f87bcc6f58fc8c7947
emex4gman <emex4gman@gmail.com>
2020-07-16 00:40:49 +0100

    auth added

--- src/routes/v1/foodit.route.js
+++ src/routes/v1/foodit.route.js
@@ -5,7 +5,7 @@ const fooditController = require('../../controllers/foodit.controller');
 
 const router = express.Router();
 
-router.get('/', fooditController.getFoods);
+router.get('/', auth(), fooditController.getFoods);
 router.post('/', auth('vendor'), fooditController.createFood);
 
 module.exports = router;
