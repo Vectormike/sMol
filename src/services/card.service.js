7996c95b316d20d305a86e8842324678c60a6421
Vectormike <victorjonah199@gmail.com>
2020-07-19 18:46:55 +0100

    fix bug

--- src/services/card.service.js
+++ src/services/card.service.js
@@ -9,26 +9,22 @@ const getCard = async () => {
 };
 
 const createCard = async (cardBody) => {
-  try {
-    const creditcard = new CreditCard(cardBody);
-    const valid = creditcard.isValid();
-    const data = creditcard.getSafeData();
-    const validation = creditcard.validate();
-    const { brand, validCardNumber, validHolder, validCvv, validExpiration, isExpired } = validation;
-    if (
-      validCardNumber === true &&
-      isExpired === false &&
-      validHolder === true &&
-      validCvv === true &&
-      validExpiration === true
-    ) {
-      const card = await Card.create(cardBody);
-      return { valid, card, data };
-    }
-    throw new ApiError(httpStatus.UNAUTHORIZED, 'Card is invalid');
-  } catch (error) {
-    return error;
+  const creditcard = new CreditCard(cardBody);
+  const valid = creditcard.isValid();
+  const data = creditcard.getSafeData();
+  const validation = creditcard.validate();
+  const { brand, validCardNumber, validHolder, validCvv, validExpiration, isExpired } = validation;
+  if (
+    validCardNumber === true &&
+    isExpired === false &&
+    validHolder === true &&
+    validCvv === true &&
+    validExpiration === true
+  ) {
+    const card = await Card.create(cardBody);
+    return { valid, card, data };
   }
+  throw new ApiError(httpStatus.UNAUTHORIZED, 'Card is invalid');
 };
 const deleteCard = async (params) => {
   const { id } = params;
