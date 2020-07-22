2f2d03d22bf22969a1834b1f9450044888160128
Vectormike <victorjonah199@gmail.com>
2020-07-22 09:53:16 +0100

    ...

--- src/routes/v1/upload.route.js
+++ src/routes/v1/upload.route.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const auth = require('../../middlewares/auth');
 const upload = require('../../middlewares/multer');
 const uploadController = require('../../controllers/upload.controller');
 
 const router = express.Router();
 
-router.route('/').post(auth('vendor'), upload.single('file'), uploadController.imageUpload);
+router.route('/').post(upload.single('file'), uploadController.imageUpload);
 
 module.exports = router;
